f531edb8b21f5fd219f9481dab88009e2b9a85eb
Fabitheguy003 <fabienmukungu@gmail.com>
2023-01-24 03:37:26 -0500

    Start od index.js

---
+++ assets/index.js
@@ -0,0 +1,27 @@
+var questionBox= [
+  {
+      question : 'Commonly used data types DO NOT Include:',
+      choice : ['strings','booleans','alerts','numbers'],
+      answer : 'booleans'
+  },
+  {
+      question : 'The condition in an if / else statement is enclosed with _______.',
+      choice : ['quotes','curly brackets','parentheses','square brackets'],                          
+      answer : 'parerntheses'
+  },
+  {
+      question : 'Arrays in javascript can be used to store _______.',
+      choice : ['numbers and strings','other ways','booleans','All of the above'],
+      answer : 'All of the above'
+  },
+  {
+      question : 'Strings values must be enclosed within ______ when being assigned to variables.',
+      choice : ['commas','curly brackets','quotes','parentheses'],
+      answer : 'curly brackets'
+  },
+  {
+      question : 'A very useful tool used during developement and debugging for printing content to the debugging is:<',
+      choice : ['javascript','terminal/bash','for loops','console.log'],
+      answer : 'for loops'
+  }
+]
